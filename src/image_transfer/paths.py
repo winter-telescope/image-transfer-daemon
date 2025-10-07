221c1dd49088938a744aaef8f840137b50d8ce50
Nathan Lourie <nlourie@mit.edu>
2025-10-07 15:37:57 -0400

    fixed path

--- src/image_transfer/paths.py
+++ src/image_transfer/paths.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).resolve().parents[1]  # adjust depth to reach repo root
-
-CONFIG_DIR = Path(PROJECT_ROOT, "config")
+PROJECT_ROOT = Path(__file__).resolve().parent  # adjust depth to reach repo root
+print(f"PROJECT_ROOT = {PROJECT_ROOT}")
+GIT_REPO_ROOT = PROJECT_ROOT.parents[
+    1
+]  # assuming this is one level up from the project root
+print(f"GIT_REPO_ROOT = {GIT_REPO_ROOT}")
+CONFIG_DIR = Path(GIT_REPO_ROOT, "config")
+print(f"CONFIG_DIR = {CONFIG_DIR}")
